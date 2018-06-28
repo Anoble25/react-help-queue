15c2c2e7a49e983a30ebe73c83fb7ac08223dca2
Alex Noble <AWGR-Alex@comcast.net>
2018-06-28 10:57:25 -0700

    finish updating state with events and binding methods lesson

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -7,12 +7,21 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState({formVisibleOnPage: true});
+    setTimeout(() => {
+      console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
+    },0);
   }
 
   render(){
     return (
       <div>
         <p>This is the NewTicketControl component!</p>
+        <strong onClick={this.handleClick}>Click me to change my state!</strong>
       </div>
     );
   }
